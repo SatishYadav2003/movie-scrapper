fdf3d8be21486c61f6a54eaefba0609251f652f0
SatishYadav2003 <sy389698@gmail.com>
2025-03-13 17:38:26 +0530

    chrome binary added

--- helper/helper_methods.py
+++ helper/helper_methods.py
@@ -33,6 +33,8 @@ chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox") 
 chrome_options.add_argument("--disable-dev-shm-usage") 
 
+chrome_options.binary_location = "/usr/bin/google-chrome-stable"
+
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service,options=chrome_options)
 
