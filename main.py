e757b67b073d5aa1904e25dca6380ffdca2bfbaa
SatishYadav2003 <sy389698@gmail.com>
2025-03-13 17:16:37 +0530

    server added

--- main.py
+++ main.py
@@ -1,24 +1,43 @@
-# import time
-# import schedule
-from helper.helper_methods import execute_driver
-from movie_scrapper import movie_scrapping_begin
+# # import time
+# # import schedule
+# from helper.helper_methods import execute_driver
+# from movie_scrapper import movie_scrapping_begin
 
 
-driver = execute_driver()
+# driver = execute_driver()
 
-def execute_movie_scrapper_fun():
-    movie_scrapping_begin(driver)
+# def execute_movie_scrapper_fun():
+#     movie_scrapping_begin(driver)
     
  
 
-execute_movie_scrapper_fun()
-
-# schedule.every(5).seconds.do(execute_movie_scrapper_fun)
+# execute_movie_scrapper_fun()
 
 
-# while True:
-#     schedule.run_pending()
-#     time.sleep(1)
+
+# # schedule.every(5).seconds.do(execute_movie_scrapper_fun)
 
 
+# # while True:
+# #     schedule.run_pending()
+# #     time.sleep(1)
+
+
+
+from fastapi import FastAPI
+import asyncio
+from helper.helper_methods import execute_driver
+from movie_scrapper import movie_scrapping_begin
+
+app = FastAPI()
+
+driver = execute_driver()
+
+async def execute_movie_scrapper_fun():
+    await asyncio.to_thread(movie_scrapping_begin, driver)  # Run in a separate thread
+
+@app.post("/start-scrape")
+async def start_scrape():
+    await execute_movie_scrapper_fun()
+    return {"message": "Scraping started!"}
 
