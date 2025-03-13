ab02bcc26c49c3ec0e7c5d0aace60ca8e330c260
SatishYadav2003 <sy389698@gmail.com>
2025-03-13 17:29:31 +0530

    gui removed

--- helper/helper_methods.py
+++ helper/helper_methods.py
@@ -30,12 +30,11 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=1920x1080")
 chrome_options.add_argument("--disable-gpu")
-
-# , options=chrome_options
-
+chrome_options.add_argument("--no-sandbox") 
+chrome_options.add_argument("--disable-dev-shm-usage") 
 
 service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service)
+driver = webdriver.Chrome(service=service,options=chrome_options)
 
 
 
